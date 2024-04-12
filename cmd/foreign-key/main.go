fa416766a113db6b6454e196b458b7e8eec3d319
Nikita-Kolbin <kolbin.niclita2014@yandex.ru>
2024-04-12 18:39:56 +0500

    init config

--- cmd/foreign-key/main.go
+++ cmd/foreign-key/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"ForeignKey/internal/config"
+)
 
+func main() {
+	cfg := config.MustLoad()
+
+	_ = cfg
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: postgres
+
+	// TODO: init router: chi, render
+
+	// TODO: init server
 }
---
+++ internal/config/config.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8082"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"10s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("connfig file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("can't read config: %s", err)
+	}
+
+	return &cfg
+}
