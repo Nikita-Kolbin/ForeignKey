34457d661ccef842bb6bdb6b664f73306b502a8d
Nikita-Kolbin <kolbin.niclita2014@yandex.ru>
2024-04-16 23:36:39 +0500

    init http router

--- cmd/foreign-key/main.go
+++ cmd/foreign-key/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"ForeignKey/internal/config"
+	"ForeignKey/internal/http-server/handlers/hello"
+	mwLogger "ForeignKey/internal/http-server/middleware/logger"
 	"ForeignKey/internal/logger"
 	"ForeignKey/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	l "log"
 	"log/slog"
+	"net/http"
 )
 
 func main() {
@@ -26,7 +31,29 @@ func main() {
 
 	_ = storage
 
-	// TODO: init router: chi, render
+	router := chi.NewRouter()
 
-	// TODO: init server
+	// middleware
+	router.Use(middleware.RequestID)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	// handlers
+	router.Get("/hello/{name}", hello.New(log))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+
+	log.Info("starting server", slog.String("addr", cfg.Address))
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 }
