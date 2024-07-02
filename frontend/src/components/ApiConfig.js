77e418d9ad46ee5a1a6cbbf8aed99f8adf526e30
anatolik-bro <anatolik.kasyanov@gmail.com>
2024-07-02 12:36:27 +0500

    .

--- frontend/src/components/ApiConfig.js
+++ frontend/src/components/ApiConfig.js
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'http://46.229.214.19:8082/api';
+const API_BASE_URL = 'http://backend:8082/api';
 
-const API_BASE_URL = 'http://localhost:8082/api';
+// const API_BASE_URL = 'http://localhost:8082/api';
 
 export { API_BASE_URL };
