cb86bbf738e274686c058e8e56eb3ff3969f705f
anatolik-bro <anatolik.kasyanov@gmail.com>
2024-07-10 18:23:17 +0500

    api

--- frontend/src/components/ApiConfig.js
+++ frontend/src/components/ApiConfig.js
@@ -1,7 +1,7 @@
 // const API_BASE_URL = 'http://backend:8082/api';
 
-// const API_BASE_URL = 'http://localhost:8082/api';
+const API_BASE_URL = 'http://localhost:8082/api';
 
-const API_BASE_URL = 'https://maestro-crm.ru/api';
+// const API_BASE_URL = 'https://maestro-crm.ru/api';
 
 export { API_BASE_URL };
