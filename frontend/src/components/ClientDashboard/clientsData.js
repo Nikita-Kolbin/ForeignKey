585a53cdd481b0ef68c2788cc78439dc198ca2b1
anatolik-bro <anatolik.kasyanov@gmail.com>
2024-07-10 12:49:14 +0500

    added the display of customer data

--- frontend/src/components/ClientDashboard/clientsData.js
+++
@@ -1,9 +0,0 @@
-// clientsData.js
-const clients = [
-    { id: 1, name: 'Иван', email: 'ivan@example.com', phone: '+1234567890' },
-    { id: 2, name: 'Петр', email: 'petr@example.com', phone: '+0987654321' },
-    // Добавьте остальных клиентов по аналогии
-  ];
-  
-  export default clients;
-  
