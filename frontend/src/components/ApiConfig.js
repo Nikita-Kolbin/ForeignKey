212717c7f48b96bbeed5dad6fa3f7f622a722476
Nikita-Kolbin <kolbin.niclita2014@yandex.ru>
2024-07-10 15:34:44 +0500

    fix api port

--- frontend/src/components/ApiConfig.js
+++ frontend/src/components/ApiConfig.js
@@ -2,6 +2,6 @@
 
 // const API_BASE_URL = 'http://localhost:8082/api';
 
-const API_BASE_URL = 'http://85.193.89.218:8082/api';
+const API_BASE_URL = 'https://maestro-crm.ru:8085/api';
 
 export { API_BASE_URL };
